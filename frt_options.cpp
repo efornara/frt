0e6dae12d33600a3cf7837b78cd18fc3c0126ae8
Emanuele Fornara <efornara@users.sourceforge.net>
2017-08-13 20:21:37 +0200

    updated version number

--- frt_options.cpp
+++ frt_options.cpp
@@ -101,7 +101,7 @@ void parse_frt_args(int argc, char **argv) {
 	for (int i = 1; i < argc; i++) {
 		const char *s = argv[i];
 		if (!strcmp(s, "-v")) {
-			printf("frt 0.9.1\n");
+			printf("frt 0.9.2\n");
 			exit(0);
 		} else if (!strcmp(s, "-h")) {
 			usage(program_name, 0);
