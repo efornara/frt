7c320eef2f1c3446aad9ec1881f6ca93e250234e
Emanuele Fornara <efornara@users.sourceforge.net>
2022-01-22 21:06:25 +0100

    backported arch and cross compilation from 2.0

--- detect.py
+++ detect.py
@@ -47,10 +47,12 @@ def get_opts():
 		def BoolVariable(a,b,c): return (a,b,c)
 
 	return [
-		('frt_arch', 'Architecture (pc/pi1/pi2/pi3/pi4/*)', 'pc'),
+		('frt_arch', 'Architecture (no/arm32v6/arm32v7/arm64v8)', 'no'),
+		('frt_cross', 'Cross compilation (no/auto/<triple>)', 'no'),
 		BoolVariable('use_llvm', 'Use llvm compiler', no),
 		BoolVariable('use_lto', 'Use link time optimization', no),
-		BoolVariable('pulseaudio', 'Detect and use pulseaudio', no),
+		BoolVariable('use_static_cpp', 'Link libgcc and libstdc++ statically', yes),
+		BoolVariable('pulseaudio', 'Detect and use pulseaudio', yes),
 	]
 
 
@@ -81,7 +83,7 @@ def configure(env):
 			env['CXX'] = 'clang++'
 			env['LD'] = 'clang++'
 		env.Append(CPPFLAGS=['-DTYPED_METHOD_BIND'])
-		env.extra_suffix = '.llvm'
+		env.extra_suffix += '.llvm'
 
 	if check(env, 'use_lto'):
 		if check(env, 'use_llvm'):
@@ -92,85 +94,61 @@ def configure(env):
 		else:
 			env.Append(CCFLAGS=['-flto'])
 			env.Append(LINKFLAGS=['-flto'])
+		env.extra_suffix += '.lto'
+
+	if env['frt_cross'] != 'no':
+		if env['frt_cross'] == 'auto':
+			triple = {
+				'arm32v7': 'arm-linux-gnueabihf',
+				'arm64v8': 'aarch64-linux-gnu',
+			}[env['frt_arch']]
+		else:
+			triple = env['frt_cross']
+		if check(env, 'use_llvm'):
+			env.Append(CCFLAGS=['-target', triple])
+			env.Append(LINKFLAGS=['-target', triple])
+		else:
+			env['CC'] = triple + '-gcc'
+			env['CXX'] = triple + '-g++'
+		pkg_config = triple + '-pkg-config'
+	else:
+		pkg_config = 'pkg-config'
 
 	env.Append(CCFLAGS=['-pipe'])
 	env.Append(LINKFLAGS=['-pipe'])
 
-	if os.system('pkg-config --exists alsa') == 0:
+	if os.system(pkg_config + ' --exists alsa') == 0:
 		print('Enabling ALSA')
 		env.Append(CPPFLAGS=['-DALSA_ENABLED'])
 		if has_wrapper_for('alsa'):
 			env['alsa'] = True
 		else:
-			env.ParseConfig('pkg-config alsa --cflags --libs')
+			env.ParseConfig(pkg_config + ' alsa --cflags --libs')
 	else:
 		print('ALSA libraries not found, disabling driver')
 
 	if check(env, 'pulseaudio'):
-		if os.system("pkg-config --exists libpulse") == 0:
-			print("Enabling PulseAudio")
-			env.Append(CPPDEFINES=["PULSEAUDIO_ENABLED"])
-			if version.major == 2:
-				env.ParseConfig("pkg-config --cflags --libs libpulse-simple")
-			elif has_wrapper_for('libpulse'):
-				env.ParseConfig("pkg-config --cflags libpulse")
-			else:
-				env.ParseConfig("pkg-config --cflags --libs libpulse")
-		else:
-			print("PulseAudio libraries not found, disabling driver")
-
-	# common
-	if not check(env, 'builtin_freetype'):
-		env.ParseConfig('pkg-config freetype2 --cflags --libs')
-	if not check(env, 'builtin_libpng'):
-		env.ParseConfig('pkg-config libpng --cflags --libs')
-	if not check(env, 'builtin_zlib'):
-		env.ParseConfig('pkg-config zlib --cflags --libs')
-	if not check(env, 'builtin_libwebp'):
-		env.ParseConfig('pkg-config libwebp --cflags --libs')
-	if not check(env, 'builtin_libtheora'):
-		env.ParseConfig('pkg-config theora theoradec --cflags --libs')
-	if not check(env, 'builtin_libvorbis'):
-		env.ParseConfig('pkg-config vorbis vorbisfile --cflags --libs')
-	if not check(env, 'builtin_opus'):
-		env.ParseConfig('pkg-config opus opusfile --cflags --libs')
-	if not check(env, 'builtin_libogg'):
-		env.ParseConfig('pkg-config ogg --cflags --libs')
-	# 2.1 / 3.0
-	if version.major == 2 or (version.major == 3 and version.minor == 0):
-		if not check(env, 'builtin_openssl') and check(env, 'module_openssl_enabled'):
-			env.ParseConfig('pkg-config openssl --cflags --libs')
-	# 3.0+
-	if version.major == 3:
-		if not check(env, 'builtin_libvpx'):
-			env.ParseConfig('pkg-config vpx --cflags --libs')
-		if not check(env, 'builtin_bullet'):
-			env.ParseConfig('pkg-config bullet --cflags --libs')
-		if not check(env, 'builtin_enet'):
-			env.ParseConfig('pkg-config libenet --cflags --libs')
-		if not check(env, 'builtin_zstd'):
-			env.ParseConfig('pkg-config libzstd --cflags --libs')
-		if not check(env, 'builtin_pcre2'):
-			env.ParseConfig('pkg-config libpcre2-32 --cflags --libs')
-	# 3.1+
-	if version.major == 3 and version.minor >= 1:
-		if not check(env, 'builtin_mbedtls') and check(env, 'module_openssl_enabled'):
-			env.Append(LIBS=['mbedtls', 'mbedcrypto', 'mbedx509'])
-		if not check(env, 'builtin_wslay') and check(env, 'module_openssl_enabled'):
-			env.ParseConfig('pkg-config libwslay --cflags --libs')
-		if not check(env, 'builtin_miniupnpc'):
-			env.Prepend(CPPPATH=["/usr/include/miniupnpc"])
-			env.Append(LIBS=["miniupnpc"])
+            if os.system(pkg_config + ' --exists libpulse') == 0:
+                    print('Enabling PulseAudio')
+                    env.Append(CPPDEFINES=['PULSEAUDIO_ENABLED'])
+                    if version.major == 2:
+                            env.ParseConfig(pkg_config + ' --cflags --libs libpulse-simple')
+                    elif has_wrapper_for('libpulse'):
+                            env.ParseConfig(pkg_config + ' --cflags libpulse')
+                    else:
+                            env.ParseConfig(pkg_config + ' --cflags --libs libpulse')
+            else:
+                    print('PulseAudio libraries not found, disabling driver')
 
 	# pkg-config returns 0 when the lib exists...
-	found_udev = not os.system('pkg-config --exists libudev')
+	found_udev = not os.system(pkg_config + ' --exists libudev')
 	if found_udev:
 		print('Enabling udev support')
 		env.Append(CPPFLAGS=['-DUDEV_ENABLED'])
 		if has_wrapper_for('udev'):
 			env.Append(FRT_MODULES=['include/libudev-so_wrap.c'])
 		else:
-			env.ParseConfig('pkg-config libudev --cflags --libs')
+			env.ParseConfig(pkg_config + ' libudev --cflags --libs')
 		env.Append(CPPFLAGS=['-DJOYDEV_ENABLED'])
 		env.Append(FRT_MODULES=['include/joypad_linux.cpp'])
 	else:
@@ -195,27 +173,21 @@ def configure(env):
 		if version.major == 2 or (version.major == 3 and version.minor == 0):
 			env.Append(CCFLAGS=['-ffast-math'])
 
-	if env['frt_arch'] == 'pi1':
-		env.Append(CCFLAGS=['-mcpu=arm1176jzf-s', '-mfpu=vfp'])
-		env.extra_suffix += '.pi1'
-	elif env['frt_arch'] == 'pi2':
-		env.Append(CCFLAGS=['-mcpu=cortex-a7', '-mfpu=neon-vfpv4'])
-		env.extra_suffix += '.pi2'
-	elif env['frt_arch'] == 'pi3':
-		env.Append(CCFLAGS=['-mcpu=cortex-a53', '-mfpu=neon-fp-armv8'])
-		env.extra_suffix += '.pi3'
-	elif env['frt_arch'] == 'pi4':
-		env.Append(CCFLAGS=['-mcpu=cortex-a72', '-mfpu=neon-fp-armv8', '-mtune=cortex-a72'])
-		env.extra_suffix += '.pi4'
-	else:
-		env.extra_suffix += '.' + env['frt_arch']
-
-	if env['frt_arch'].startswith('pi'):
-		env.Append(CCFLAGS=['-mfloat-abi=hard', '-mlittle-endian', '-munaligned-access'])
+	if env['frt_arch'] == 'arm32v6':
+		env.Append(CCFLAGS=['-march=armv6', '-mfpu=vfp', '-mfloat-abi=hard'])
+		env.extra_suffix += '.arm32v6'
+	elif env['frt_arch'] == 'arm32v7':
+		env.Append(CCFLAGS=['-march=armv7-a', '-mfpu=neon-vfpv4', '-mfloat-abi=hard'])
+		env.extra_suffix += '.arm32v7'
+	elif env['frt_arch'] == 'arm64v8':
+		env.Append(CCFLAGS=['-march=armv8-a'])
+		env.extra_suffix += '.arm64v8'
 
 	env.Append(CFLAGS=['-std=gnu11']) # for libwebp (maybe more in the future)
 	env.Append(CPPFLAGS=['-DFRT_ENABLED', '-DUNIX_ENABLED', '-DGLES2_ENABLED', '-DGLES_ENABLED'])
 	env.Append(LIBS=['pthread'])
+	if env['frt_arch'] == 'arm32v6' and version.major == 3 and version.minor >= 4: # TODO find out exact combination
+		env.Append(LIBS=['atomic'])
 
 	env.Append(FRT_MODULES=['envprobe.cpp'])
 	env.Append(FRT_MODULES=['video_fbdev.cpp', 'keyboard_linux_input.cpp', 'mouse_linux_input.cpp'])
