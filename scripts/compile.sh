0ef7a555104c34f73471b02b03f5bbcc3f18a1fe
Emanuele Fornara <efornaralabs@gmail.com>
2025-09-13 11:31:10 +0200

    compile.sh

---
+++ scripts/compile.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+set -e
+
+# compile.sh
+#
+# FRT - A Godot platform targeting single board computers
+# Copyright (c) 2017-2025  Emanuele Fornara
+# SPDX-License-Identifier: MIT
+#
+
+BASEDIR=`pwd`
+
+if [ -d pupazzi ] ; then
+	cd pupazzi
+	git pull
+else
+	git clone -b frt https://github.com/efornara/pupazzi
+fi
+
+cd ${BASEDIR}/pupazzi/platform
+if [ -d frt ] ; then
+	cd frt
+	git pull
+else
+	git clone https://github.com/efornara/frt
+fi
+
+cd ${BASEDIR}/pupazzi
+
+OPTIONS="
+	platform=frt
+	tools=no
+	module_webm_enabled=no
+	verbose=yes
+	warnings=no
+	progress=no
+	production=yes
+	LINKFLAGS=-s
+"
+
+PATH=${GODOT_SDK_LINUX_ARM64}/bin:${SDL2_ARM64}/bin:${BASE_PATH}
+scons ${OPTIONS} arch=arm64 target=release
+
+PATH=${GODOT_SDK_LINUX_ARM32}/bin:${SDL2_ARM32}/bin:${BASE_PATH}
+scons ${OPTIONS} arch=arm32 target=release
